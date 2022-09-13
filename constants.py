7a359bf9b7e3bc7fe214866899a9b1581868288a
Julián Chimente <julichimente@gmail.com>
2022-09-13 00:47:50 -0300

    Add files via upload
    Constantes necesarias para programa

---
+++ constants.py
@@ -0,0 +1,5 @@
+# Constantes
+URL = 'https://es.wikipedia.org/wiki/Anexo:Estad%C3%ADsticas_de_la_Liga_de_Campeones_de_la_UEFA'
+CANTIDAD_JUGADORES = 30
+COLUMNAS_DATAFRAME = ['Jugador', 'Url_Bandera', 'Posicion']
+RUTA_XPATH_BASE = '//*[@id="mw-content-text"]/div[1]/table[9]/tbody/tr[*]/td'
